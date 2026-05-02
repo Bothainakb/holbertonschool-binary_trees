3410f8c69ded2c71af6718f2e6cda21d8a39ea68
dana <trivian1966@gmail.com>
2026-05-02 14:49:47 +0000

    Correcting Task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,34 +1,23 @@
 #include "binary_trees.h"
 
 /**
- * _height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree.
+ * binary_tree_leaves - Counts the leaves in a binary tree
+ * @tree: A pointer to the root node of the tree to count the leaves
  *
- * Return: The height of the tree.
+ * Return: The number of leaves, or 0 if tree is NULL
  */
-size_t _height(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t left_h = 0, right_h = 0;
+	size_t leaves = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left_h = _height(tree->left);
-	right_h = _height(tree->right);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-	return ((left_h > right_h ? left_h : right_h) + 1);
-}
+	leaves += binary_tree_leaves(tree->left);
+	leaves += binary_tree_leaves(tree->right);
 
-/**
- * binary_tree_balance - Measures the balance factor of a binary tree.
- * @tree: A pointer to the root node of the tree to measure.
- *
- * Return: The balance factor, or 0 if tree is NULL.
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	return ((int)_height(tree->left) - (int)_height(tree->right));
+	return (leaves);
 }
